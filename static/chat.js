0b2fc55e9077eee90a1372ce585798b4768a1b12
Debjit Pal <debjitpal5040@gmail.com>
2023-05-17 23:25:47 +0530

    SpeechRecognition

--- static/chat.js
+++ static/chat.js
@@ -13,10 +13,11 @@ function getBotResponse() {
         voices = window.speechSynthesis.getVoices();
         speech.voice = voices[1];
         speech.text = data;
-        speech.rate = 0.5;
+        speech.rate = 0.75;
+        speech.volume = 2;
+        speech.pitch = 1.5;
         window.speechSynthesis.speak(speech);
         document.getElementById('userInput').scrollIntoView({ block: 'start', behavior: 'smooth' });
-
     });
 }
 $("#textInput").keypress(function (e) {
@@ -26,4 +27,21 @@ $("#textInput").keypress(function (e) {
 });
 $("#buttonInput").click(function () {
     getBotResponse();
+})
+
+// speech recognition api
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = "en-IN";
+recognition.onstart = function () {
+    console.log("voice is activated, you can speak to microphone");
+}
+recognition.onresult = function (event) {
+    const current = event.resultIndex;
+    const transcript = event.results[current][0].transcript;
+    $("#textInput").val(transcript);
+    getBotResponse();
+}
+$("#microphone").click(function () {
+    recognition.start();
 })
